830ce86598748dae6cc85c62ce7ba80a974d93a2
FinoanaRas <finoanarsl@gmail.com>
2023-03-07 09:47:40 +0300

    sprint2

--- webFramework/src/main/java/etu2054/framework/servlet/FrontServlet.java
+++ webFramework/src/main/java/etu2054/framework/servlet/FrontServlet.java
@@ -1,16 +1,21 @@
 package etu2054.framework.servlet;
 
+import etu2054.framework.Mapping;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 @WebServlet(name = "FrontServlet", value = "/*")
 public class FrontServlet extends HttpServlet {
+    HashMap<String, Mapping> mappingUrls;
     public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
-        out.println("hello");
+
+        out.println("hello "+request.getPathInfo());
     }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
