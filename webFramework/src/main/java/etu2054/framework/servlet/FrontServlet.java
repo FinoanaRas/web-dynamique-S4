022cdfe5bfdd248b37802823c399480ccf44616f
FinoanaRas <finoanarsl@gmail.com>
2023-03-10 11:28:43 +0300

    Revert "sprint2"
    This reverts commit 830ce86598748dae6cc85c62ce7ba80a974d93a2.

--- webFramework/src/main/java/etu2054/framework/servlet/FrontServlet.java
+++ webFramework/src/main/java/etu2054/framework/servlet/FrontServlet.java
@@ -1,21 +1,16 @@
 package etu2054.framework.servlet;
 
-import etu2054.framework.Mapping;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
 
 @WebServlet(name = "FrontServlet", value = "/*")
 public class FrontServlet extends HttpServlet {
-    HashMap<String, Mapping> mappingUrls;
     public void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
-
-        out.println("hello "+request.getPathInfo());
+        out.println("hello");
     }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
