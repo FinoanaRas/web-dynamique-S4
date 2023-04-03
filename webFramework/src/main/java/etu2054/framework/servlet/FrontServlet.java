801e8eacba3028c22e1e89886efbe0041de232ac
Finoana Rasolomalala <finoanarsl@gmail.com>
2023-04-03 13:11:08 +0400

    Revert "sprint-4"
    This reverts commit 66981b851a3b0a5a8cd63ea8e3693e9de9820c56.

--- webFramework/src/main/java/etu2054/framework/servlet/FrontServlet.java
+++ webFramework/src/main/java/etu2054/framework/servlet/FrontServlet.java
@@ -2,6 +2,7 @@ package etu2054.framework.servlet;
 
 import etu2054.framework.Mapping;
 import etu2054.framework.annotations.UrlAnnot;
+import etu2054.framework.model.Personne;
 import etu2054.framework.util.StaxParser;
 
 import javax.servlet.*;
@@ -9,7 +10,6 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
 import java.net.URL;
@@ -27,9 +27,7 @@ public class FrontServlet extends HttpServlet {
         File directory = null;
         try {
             StaxParser staxParser = new StaxParser();
-            ServletContext servletContext = getServletContext();
-            InputStream in = servletContext.getResourceAsStream("/WEB-INF/webConfig.xml");
-            String path = staxParser.getConfig(in);
+            String path = staxParser.getConfig("/configWeb.xml");
             ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
             if (classLoader == null) {
                 throw new ClassNotFoundException("Can't get class loader.");
